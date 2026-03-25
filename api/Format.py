ca4ce99651424849c1ae925a51b23d51789582e0
Sheshaya <bncdhff@gmail.com>
2026-03-26 01:21:30 +0300

    ML & XAI

--- api/Format.py
+++ api/Format.py
@@ -1,7 +1,7 @@
 # Data validation and formatting for the insurance model API
 
 from pydantic import BaseModel, Field
-from typing import Literal
+from typing import Literal, Dict
 
 class DataRequest(BaseModel):
     age: int = Field(..., ge=0, le=120, description="Age of the Patient")
@@ -12,5 +12,8 @@ class DataRequest(BaseModel):
     region: Literal["northeast", "northwest", "southeast", "southwest"]
 
 class PredictionResponse(BaseModel):
-    PredictedCost: float = Field(..., ge=0, description="Predicted Insurance Cost")
-    
+    Predicted_Cost: float = Field(..., ge=0, description="Predicted Insurance Cost")
+
+class PermutationImportanceResponse(BaseModel):
+    Feature_Importance: Dict[str, float] = Field(..., description="Permutation importance scores for each feature")
+    Engineering_Features: Dict[str, float] = Field(..., description="Permutation importance scores for engineered features")
--- api/main.py
+++ api/main.py
@@ -7,7 +7,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
-from api.Format import DataRequest, PredictionResponse
+from api.Format import DataRequest, PredictionResponse, PermutationImportanceResponse
 from api.ModelUtils import feature_engineering
 import api.CostumModel
 
@@ -60,8 +60,27 @@ async def predict(patient_data: DataRequest):
         df = feature_engineering(patient_data)
         X = model_component["InsurancePreprocessor"].transform(df)
         predicted_cost = model_component["InsuranceModel"].predict(X)[0]
-        return PredictionResponse(PredictedCost=predicted_cost)
+        return PredictionResponse(Predicted_Cost=predicted_cost)
     except ValueError as verr:
         raise HTTPException(status_code=400, detail=f"Invalid input data: {verr}")
     except Exception as err:
         raise HTTPException(status_code=500, detail=f"Prediction error: {err}")
+    
+@app.get("/permutation_importance", response_model=PermutationImportanceResponse) # permutation importance endpoint
+async def PermutationImportance():    # Avarage Permutation importance scores from jupyter
+    return PermutationImportanceResponse(
+        Feature_Importance={
+            "smoker": 1.4149,
+            "bmi": 0.2875,
+            "age": 0.1994,
+            "children": 0.0054,
+            "sex": 0.0002,
+            "region": -0.0003,
+        },
+        Engineering_Features={
+            "bmi_smoker": 0.8739,
+            "bmi_age": 0.0251,
+            "age_smoker": 0.0244,
+            "smoker_yes": 0.0112,
+            "no_children": 0.0004,
+        })
